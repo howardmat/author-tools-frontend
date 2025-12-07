343c960312486a7c54b7f3a92f84c8b2bfe4e14a
Mat Howard <howard.mat@gmail.com>
2025-12-07 11:07:22 -0500

    Resolve lint issue

--- src/store/workspace/workspace-context-provider.tsx
+++ src/store/workspace/workspace-context-provider.tsx
@@ -61,6 +61,7 @@ const WorkspaceContextProvider = ({ children }: PropsWithChildren) => {
         }
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSuccess, workspaces]);
 
   return (
