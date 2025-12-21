0c63d54b81da3506e79b181cf665bf6dab511fe7
Mat Howard <howard.mat@gmail.com>
2025-12-21 10:57:11 -0500

    Fix bug with sign-up sending two verification codes

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -5,11 +5,7 @@ export default function SignInPage() {
     <div className='flex min-h-full flex-1'>
       <div className='flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
         <div className='mx-auto w-full max-w-sm lg:w-96'>
-          <SignIn
-            path='/sign-in'
-            fallbackRedirectUrl='/characters'
-            signUpUrl='/sign-up'
-          />
+          <SignIn fallbackRedirectUrl='/characters' signUpUrl='/sign-up' />
         </div>
       </div>
       <div className='relative hidden w-0 flex-1 lg:block'>
--- src/pages/sign-up-page.tsx
+++ src/pages/sign-up-page.tsx
@@ -1,15 +1,12 @@
 import { SignUp } from '@clerk/clerk-react';
 
 export default function SignUpPage() {
+  console.log('Rendering SignUpPage');
   return (
     <div className='flex min-h-full flex-1'>
       <div className='flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
         <div className='mx-auto w-full max-w-sm lg:w-96'>
-          <SignUp
-            path='/sign-up'
-            fallbackRedirectUrl='/characters'
-            signInUrl='/sign-in'
-          />
+          <SignUp fallbackRedirectUrl='/characters' signInUrl='/sign-in' />
         </div>
       </div>
       <div className='relative hidden w-0 flex-1 lg:block'>
